270d58dfc0cbd8487842e69c5cbce3c2c25052b8
OM JADHAV <114276120+OmJadhav1@users.noreply.github.com>
2022-12-21 00:03:37 +0530

    Time: 7 ms (14.77%), Space: 7.2 MB (35.56%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -4,16 +4,28 @@ class Solution
         void rotate(vector<vector < int>> &matrix)
         {
             int n = matrix.size();
-            vector<vector < int>> rotated(n, vector<int> (n, 0));
+           	//transposing the matrix
             for (int i = 0; i < n; i++)
             {
-                for (int j = 0; j < n; j++)
+                for (int j = 0; j < i; j++)
                 {
-                    rotated[j][n - i - 1] = matrix[i][j];
+                    swap(matrix[i][j], matrix[j][i]);
                 }
             }
-            matrix = rotated;
+           	//reversing each row of the matrix
+            for (int i = 0; i < n; i++)
+            {
+                reverse(matrix[i].begin(), matrix[i].end());
+            }
         }
 };
-//Brute force
-/*Take another dummy matrix of n*n, and then take the first row of the matrix and put it in the last column of the dummy matrix, take the second row of the matrix, and put it in the second last column of the matrix and so.*/
+
+//Optimized approach
+
+/*Intuition: By observation, we see that the first column of the original matrix is the reverse of the first row of the rotated matrix, so that’s why we transpose the matrix and then reverse each row, and since we are making changes in the matrix itself space complexity gets reduced to O(1).*/
+
+/*Approach:
+
+Step1: Transpose of the matrix. (transposing means changing columns to rows and rows to columns)
+
+Step2: Reverse each row of the matrix.*/
