bdf0a4046060cb700b9a0cc16a5e056718482644
OM JADHAV <114276120+OmJadhav1@users.noreply.github.com>
2023-10-21 15:20:24 +0530

    Time: 3 ms (87.67%), Space: 9.2 MB (33.98%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        //brute force
-        int n1= nums1.size();
-        int n2= nums2.size();
+        int n1=nums1.size();
+        int n2=nums2.size();
         vector<int> ans(n1,-1);
-        for(int i=0; i<n1;i++){
-            int find=-1;
-            for(int j=0;j<n2;j++){
-                if(nums1[i] == nums2[j]){
-                    find=j;
-                    break;
-                }
+        stack<int> s;
+        unordered_map<int,int> m;
+        
+        for(int i=0;i<n2;i++){
+            while(!s.empty() && s.top()<nums2[i]){
+                m[s.top()] = nums2[i];
+                s.pop();
             }
-            for(int j=find;j<n2;j++){
-                if(nums2[j]> nums1[i]){
-                    ans[i] = nums2[j];
-                    break;
-                }
+            s.push(nums2[i]);
+        }
+        
+        for(int i=0;i<n1;i++){
+            if(m[nums1[i]]){
+                ans[i] = m[nums1[i]];
             }
         }
         return ans;
