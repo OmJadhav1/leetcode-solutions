b83f944bfeba2ec39c7d5dd2ccc6830dfca0931e
OM JADHAV <114276120+OmJadhav1@users.noreply.github.com>
2024-04-29 11:01:04 +0530

    Time: 11 ms (69.87%) | Memory: 17.3 MB (35.73%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -1,20 +1,16 @@
 class Solution {
 public:
-    static auto comp(pair<int,int>a, pair<int,int> b){
-        return a.second>=b.second;
-    }
     vector<int> topKFrequent(vector<int>& nums, int k) {
         unordered_map<int,int> m;
-        vector<pair<int,int>> vec;
         vector<int> ans;
         for(int x: nums)
             m[x]++;
 
-        for(auto x: m){
-            vec.push_back({x.first,x.second});
-        }
+        vector<pair<int,int>> vec(m.begin(),m.end());
 
-        sort(vec.begin(),vec.end(), comp);
+        sort(vec.begin(),vec.end(), []( pair<int,int>a, pair<int,int> b){
+            return a.second>b.second;
+        });
 
         for(int i=0;i<k;i++){
             ans.push_back(vec[i].first);
